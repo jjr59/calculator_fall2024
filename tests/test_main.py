4300d54e9f092375d86b1a4b59c8fad02108a488
jjr59 <jjr59@njit.edu>
2024-09-30 14:18:14 -0400

    added division and testing for divide by zero

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,25 @@
 '''My Calculator Test'''
-from app.main import addition, subtraction
+import pytest
+from app.main import addition, division, multiplication, subtraction
 
 def test_addition():
     '''Addition function'''
     assert addition(1,1) == 2
 
 def test_subtraction():
-        assert subtraction(1,1) == 0
+    '''Subtraction function'''
+    assert subtraction(1,1) == 0
+
+def test_multiplication():
+    '''Multiplication function'''
+    assert multiplication(1,2) == 2
+
+def test_positive_division():
+    '''Positive Division Test'''
+    assert division(1,1) == 1
+
+def test_negative_division():
+    '''Negatvie Division Test'''
+    with pytest.raises(ZeroDivisionError):
+        division(10 , 0)
+        
