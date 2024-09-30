52e058d3dd6afc0e8541986e33eaade72f707eee
jjr59 <jjr59@njit.edu>
2024-09-30 13:32:10 -0400

    added multiplication and test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,15 @@
 '''My Calculator Test'''
-from app.main import addition, subtraction
+from app.main import addition, multiplication, subtraction
 
 def test_addition():
     '''Addition function'''
     assert addition(1,1) == 2
 
 def test_subtraction():
+        '''Subtraction function'''
         assert subtraction(1,1) == 0
+
+def test_multiplication():
+        '''Multiplication function'''
+        assert multiplication(1,2) == 2
+
